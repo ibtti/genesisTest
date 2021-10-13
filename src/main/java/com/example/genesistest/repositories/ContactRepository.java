8ca83ef74103702d29a939b959085b10428edc5b
ibtti <ibtibouallegue@gmail.com>
2021-10-13 08:30:01 +0200

    Test Genesis

--- src/main/java/com/example/genesistest/repositories/ContactRepository.java
+++ src/main/java/com/example/genesistest/repositories/ContactRepository.java
@@ -2,6 +2,8 @@ package com.example.genesistest.repositories;
 
 import com.example.genesistest.entities.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<ContactEntity,String> {
 }
